baec0e0f46e28048d8d46e644402352af94a6eb9
87001 <870011410@qq.com>
2019-04-19 15:06:21 +0800

    修改测试

--- CrowdscourcingPlatform.web/CrowdscourcingPlatform.web.front/src/main/java/org/crowd/controller/LoginController.java
+++ CrowdscourcingPlatform.web/CrowdscourcingPlatform.web.front/src/main/java/org/crowd/controller/LoginController.java
@@ -1,11 +1,6 @@
 package org.crowd.controller;
 
-import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.sound.midi.SysexMessage;
-
+import com.alibaba.fastjson.JSONObject;
 import org.crowd.model.User;
 import org.crowd.rsa.Md5;
 import org.crowd.rsa.Rsa;
@@ -17,201 +12,205 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSONObject;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * 
  * <p>
  * Title : LoginController
  * </p>
- * 
+ *
  * <p>
  * Description :
  * </p>
- * 
+ *
  * <p>
  * DevelopTools : Eclipse_x64_v4.9.0
  * </p>
- * 
+ *
  * <p>
  * DevelopSystem : Windows10
  * </p>
- * 
+ *
  * <p>
  * Company : org.wf
  * </p>
- * 
+ *
  * @author : WuFan
- * 
- * @date : 2018年12月9日 下午10:22:17
- * 
  * @version : 12.0.0
+ * @date : 2018年12月9日 下午10:22:17
  */
 @Controller
 public class LoginController {
 
-	@Autowired
-	private LoginService loginService;
+    @Autowired
+    private LoginService loginService;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	private JSONObject outJson = new JSONObject();
+    private JSONObject outJson = new JSONObject();
 
-	// 登录
-	@RequestMapping("/do-login.action")
-	@ResponseBody
-	public JSONObject doLogin(String acc, String pwd, String code, HttpServletRequest req) {
+    // 登录
+    @RequestMapping("/do-login.action")
+    @ResponseBody
+    public JSONObject doLogin(String acc, String pwd, String code, HttpServletRequest req) {
 
-		if (acc.length() <= 0 || pwd.length() <= 0 || code.length() <= 0) {
-			// 用json
-			outJson.put("type", "illegal");
-			return outJson;
-		}
+        if (acc.length() <= 0 || pwd.length() <= 0 || code.length() <= 0) {
+            // 用json
+            outJson.put("type", "illegal");
+            return outJson;
+        }
 
-		// 判断验证码
-		if (RanImage.code.toLowerCase().equals(code.toLowerCase()) == false) {
-			// 用json
-			outJson.put("type", "codeWrong");
-			return outJson;
-		}
 
-		//解密密码再加密成md5
-		//解密密码
-		try {
-			byte[] decrypt = Rsa.decryptByPrivateKey(pwd,RsaController.privateKey);
-			pwd=new String(decrypt);
-			//加密成md5
-			pwd=Md5.encryption(pwd+pwd.substring(0,1));
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			outJson.put("type", "fail");
-			return outJson;
-		}
-		User user = new User();
-		try {
 
-			user = loginService.LoginServer(acc, pwd);
 
-		} catch (Exception e) {
-			// 用json
-			outJson.put("type", "fail");
-			return outJson;
-		}
+        // 判断验证码
+        if (RanImage.code.toLowerCase().equals(code.toLowerCase()) == false) {
+            // 用json
+            outJson.put("type", "codeWrong");
+            return outJson;
+        }
 
-		// 存到session
-		HttpSession session = req.getSession();
-		session.setAttribute("user", user);
+        //解密密码再加密成md5
+        //解密密码
+        try {
+            byte[] decrypt = Rsa.decryptByPrivateKey(pwd, RsaController.privateKey);
+            pwd = new String(decrypt);
+            //加密成md5
+            pwd = Md5.encryption(pwd + pwd.substring(0, 1));
+        } catch (Exception e1) {
+            // TODO Auto-generated catch block
+            outJson.put("type", "fail");
+            return outJson;
+        }
+        User user = new User();
+        try {
 
-		// 用json
-		outJson.put("type", "success");
-		outJson.put("user", user);
-		return outJson;
-	}
+            user = loginService.LoginServer(acc, pwd);
 
-	// 验证码
-	@RequestMapping("/do-ranImage.action")
-	public void imageCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ImageIO.write(RanImage.getVerification(), "png", response.getOutputStream()); // 将图片验证码输出
-	}
 
-	// 注册
-	@RequestMapping("/do-reg.action")
-	@ResponseBody
-	public JSONObject doReg(String acc, String pwd, String repwd, String name, String phone, String email) {
+        } catch (Exception e) {
+            // 用json
+            outJson.put("type", "fail");
+            return outJson;
+        }
 
-		// 违法操作
-		if (acc.length() <= 0 || pwd.length() <= 0 || repwd.length() <= 0 || name.length() <= 0 || phone.length() <= 0
-				|| email.length() <= 0) {
-			// 用json
-			outJson.put("type", "illegal");
-			return outJson;
-		}
-      //解密重复密码和密码
-      byte[] d;
-	try {
-		//解密密码
-		d = Rsa.decryptByPrivateKey(pwd,RsaController.privateKey);
-		pwd=new String(d);
-		//解密重复密码
-		d = Rsa.decryptByPrivateKey(repwd,RsaController.privateKey);
-		repwd=new String(d);
-	} catch (Exception e2) {
-		// TODO Auto-generated catch block
-		outJson.put("type", "fail");
-		return outJson;
-	}
-		// 重复密码错误
-		if (pwd.equals(repwd) == false) {
-			// 用json
-			outJson.put("type", "repwdWrong");
-			return outJson;
-		}
+        // 存到session
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user);
 
-		// 账号重复
-		if (userService.checkUserAcc(acc) > 0) {
-			// 用json
-			outJson.put("type", "accRepeat");
-			return outJson;
-		}
+        // 用json
+        outJson.put("type", "success");
+        outJson.put("user", user);
+        return outJson;
+    }
 
-		// 昵称重复
-		if (userService.checkRegName(name) > 0) {
-			// 用json
-			outJson.put("type", "nameRepeat");
-			return outJson;
-		}
+    // 验证码
+    @RequestMapping("/do-ranImage.action")
+    public void imageCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ImageIO.write(RanImage.getVerification(), "png", response.getOutputStream()); // 将图片验证码输出
+    }
+
+    // 注册
+    @RequestMapping("/do-reg.action")
+    @ResponseBody
+    public JSONObject doReg(String acc, String pwd, String repwd, String name, String phone, String email) {
+
+        // 违法操作
+        if (acc.length() <= 0 || pwd.length() <= 0 || repwd.length() <= 0 || name.length() <= 0 || phone.length() <= 0
+                || email.length() <= 0) {
+            // 用json
+            outJson.put("type", "illegal");
+            return outJson;
+        }
+        //解密重复密码和密码
+        byte[] d;
+        try {
+            //解密密码
+            d = Rsa.decryptByPrivateKey(pwd, RsaController.privateKey);
+            pwd = new String(d);
+            //解密重复密码
+            d = Rsa.decryptByPrivateKey(repwd, RsaController.privateKey);
+            repwd = new String(d);
+        } catch (Exception e2) {
+            // TODO Auto-generated catch block
+            outJson.put("type", "fail");
+            return outJson;
+        }
+        // 重复密码错误
+        if (pwd.equals(repwd) == false) {
+            // 用json
+            outJson.put("type", "repwdWrong");
+            return outJson;
+        }
+
+        // 账号重复
+        if (userService.checkUserAcc(acc) > 0) {
+            // 用json
+            outJson.put("type", "accRepeat");
+            return outJson;
+        }
+
+        // 昵称重复
+        if (userService.checkRegName(name) > 0) {
+            // 用json
+            outJson.put("type", "nameRepeat");
+            return outJson;
+        }
         //密码再加密成md5
-		try {
-			pwd=Md5.encryption(pwd+pwd.substring(0,1));
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			outJson.put("type", "fail");
-			return outJson;
-		}
-		// 注册
-		try {
-			userService.reg(acc, pwd, name, phone, email);
-		} catch (Exception e) {
-			// 用json
-			outJson.put("type", "fail");
-			return outJson;
-		}
+        try {
+            pwd = Md5.encryption(pwd + pwd.substring(0, 1));
+        } catch (Exception e1) {
+            // TODO Auto-generated catch block
+            outJson.put("type", "fail");
+            return outJson;
+        }
+        // 注册
+        try {
+            userService.reg(acc, pwd, name, phone, email);
+        } catch (Exception e) {
+            // 用json
+            outJson.put("type", "fail");
+            return outJson;
+        }
 
-		// 用json
-		outJson.put("type", "success");
-		return outJson;
-	}
+        // 用json
+        outJson.put("type", "success");
+        return outJson;
+    }
 
-	// 成为服务商
-	@RequestMapping("/do-beProvider.action")
-	@ResponseBody
-	public JSONObject doBeProvider(Integer userId, Integer providerId, String userName, HttpServletRequest req) {
+    // 成为服务商
+    @RequestMapping("/do-beProvider.action")
+    @ResponseBody
+    public JSONObject doBeProvider(Integer userId, Integer providerId, String userName, HttpServletRequest req) {
 
-		try {
-			userService.beProvider(userId, providerId, userName);
-		} catch (Exception e) {
-			// 用json
-			outJson.put("type", "fail");
-			return outJson;
-		}
+        try {
+            userService.beProvider(userId, providerId, userName);
+        } catch (Exception e) {
+            // 用json
+            outJson.put("type", "fail");
+            return outJson;
+        }
 
-		User user = new User();
-		user = userService.afterUpdate(userId);
-		// 存到session
-		HttpSession session = req.getSession();
-		session.setAttribute("user", user);
-		// 用json
-		outJson.put("type", "success");
-		outJson.put("user", user);
-		return outJson;
-	}
+        User user = new User();
+        user = userService.afterUpdate(userId);
+        // 存到session
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user);
+        // 用json
+        outJson.put("type", "success");
+        outJson.put("user", user);
+        return outJson;
+    }
 
-	public JSONObject getOutJson() {
-		return outJson;
-	}
+    public JSONObject getOutJson() {
+        return outJson;
+    }
 
-	public void setOutJson(JSONObject outJson) {
-		this.outJson = outJson;
-	}
+    public void setOutJson(JSONObject outJson) {
+        this.outJson = outJson;
+    }
 }
--- CrowdscourcingPlatform.web/CrowdscourcingPlatform.web.front/src/main/java/org/crowd/model/User.java
+++ CrowdscourcingPlatform.web/CrowdscourcingPlatform.web.front/src/main/java/org/crowd/model/User.java
@@ -1,5 +1,11 @@
 package org.crowd.model;
 
+import lombok.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
 	private Integer id; // 用户ID
@@ -16,159 +22,159 @@ public class User {
 	private String memo; // 自我介绍
 	private String src;// 头像
 
-	public User() {
-		// TODO Auto-generated constructor stub
-	}
+//	public User() {
+//		// TODO Auto-generated constructor stub
+//	}
 
-	public User(Integer id, String acc, String pwd, String name, String phone, String email, double money,
-			Integer score, Integer type, Integer state, String createTime, String memo, String src) {
-		super();
-		this.id = id;
-		this.acc = acc;
-		this.pwd = pwd;
-		this.name = name;
-		this.phone = phone;
-		this.email = email;
-		this.money = money;
-		this.score = score;
-		this.type = type;
-		this.state = state;
-		this.createTime = createTime;
-		this.memo = memo;
-		this.src = src;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getAcc() {
-		return acc;
-	}
-
-	public void setAcc(String acc) {
-		this.acc = acc;
-	}
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public double getMoney() {
-		return money;
-	}
-
-	public void setMoney(double money) {
-		this.money = money;
-	}
-
-	public Integer getScore() {
-		return score;
-	}
-
-	public void setScore(Integer score) {
-		this.score = score;
-	}
-
-	public Integer getType() {
-		return type;
-	}
-
-	public void setType(Integer type) {
-		this.type = type;
-	}
-
-	public Integer getState() {
-		return state;
-	}
-
-	public void setState(Integer state) {
-		this.state = state;
-	}
-
-	public String getCreateTime() {
-		return createTime;
-	}
-
-	public void setCreateTime(String createTime) {
-		this.createTime = createTime;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("User [id=");
-		builder.append(id);
-		builder.append(", acc=");
-		builder.append(acc);
-		builder.append(", pwd=");
-		builder.append(pwd);
-		builder.append(", name=");
-		builder.append(name);
-		builder.append(", phone=");
-		builder.append(phone);
-		builder.append(", email=");
-		builder.append(email);
-		builder.append(", money=");
-		builder.append(money);
-		builder.append(", score=");
-		builder.append(score);
-		builder.append(", type=");
-		builder.append(type);
-		builder.append(", state=");
-		builder.append(state);
-		builder.append(", createTime=");
-		builder.append(createTime);
-		builder.append("]");
-		return builder.toString();
-	}
-
-	public String getMemo() {
-		return memo;
-	}
-
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
-
-	public String getSrc() {
-		return src;
-	}
-
-	public void setSrc(String src) {
-		this.src = src;
-	}
+//	public User(Integer id, String acc, String pwd, String name, String phone, String email, double money,
+//			Integer score, Integer type, Integer state, String createTime, String memo, String src) {
+//		super();
+//		this.id = id;
+//		this.acc = acc;
+//		this.pwd = pwd;
+//		this.name = name;
+//		this.phone = phone;
+//		this.email = email;
+//		this.money = money;
+//		this.score = score;
+//		this.type = type;
+//		this.state = state;
+//		this.createTime = createTime;
+//		this.memo = memo;
+//		this.src = src;
+//	}
+//
+//	public Integer getId() {
+//		return id;
+//	}
+//
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//
+//	public String getAcc() {
+//		return acc;
+//	}
+//
+//	public void setAcc(String acc) {
+//		this.acc = acc;
+//	}
+//
+//	public String getPwd() {
+//		return pwd;
+//	}
+//
+//	public void setPwd(String pwd) {
+//		this.pwd = pwd;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getPhone() {
+//		return phone;
+//	}
+//
+//	public void setPhone(String phone) {
+//		this.phone = phone;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public double getMoney() {
+//		return money;
+//	}
+//
+//	public void setMoney(double money) {
+//		this.money = money;
+//	}
+//
+//	public Integer getScore() {
+//		return score;
+//	}
+//
+//	public void setScore(Integer score) {
+//		this.score = score;
+//	}
+//
+//	public Integer getType() {
+//		return type;
+//	}
+//
+//	public void setType(Integer type) {
+//		this.type = type;
+//	}
+//
+//	public Integer getState() {
+//		return state;
+//	}
+//
+//	public void setState(Integer state) {
+//		this.state = state;
+//	}
+//
+//	public String getCreateTime() {
+//		return createTime;
+//	}
+//
+//	public void setCreateTime(String createTime) {
+//		this.createTime = createTime;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		StringBuilder builder = new StringBuilder();
+//		builder.append("User [id=");
+//		builder.append(id);
+//		builder.append(", acc=");
+//		builder.append(acc);
+//		builder.append(", pwd=");
+//		builder.append(pwd);
+//		builder.append(", name=");
+//		builder.append(name);
+//		builder.append(", phone=");
+//		builder.append(phone);
+//		builder.append(", email=");
+//		builder.append(email);
+//		builder.append(", money=");
+//		builder.append(money);
+//		builder.append(", score=");
+//		builder.append(score);
+//		builder.append(", type=");
+//		builder.append(type);
+//		builder.append(", state=");
+//		builder.append(state);
+//		builder.append(", createTime=");
+//		builder.append(createTime);
+//		builder.append("]");
+//		return builder.toString();
+//	}
+//
+//	public String getMemo() {
+//		return memo;
+//	}
+//
+//	public void setMemo(String memo) {
+//		this.memo = memo;
+//	}
+//
+//	public String getSrc() {
+//		return src;
+//	}
+//
+//	public void setSrc(String src) {
+//		this.src = src;
+//	}
 
 }
